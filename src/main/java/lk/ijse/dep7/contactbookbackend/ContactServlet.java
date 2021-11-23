96067b361ab5744f70733c05405aec2a79baaccc
Manoj Randeni <randeni.ramr@gmail.com>
2021-11-23 12:56:30 +0530

    Migrate to @Resource annotation for injecting the pool

--- src/main/java/lk/ijse/dep7/contactbookbackend/ContactServlet.java
+++ src/main/java/lk/ijse/dep7/contactbookbackend/ContactServlet.java
@@ -1,16 +1,38 @@
 package lk.ijse.dep7.contactbookbackend;
 
+import jakarta.annotation.Resource;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 import java.io.IOException;
+import java.sql.SQLException;
 
 @MultipartConfig(
         location = "/home/manoj/Documents/MRR/IJSE/DEP/Phase - 2/Day_89 (2021-10-11)/contact-book-back-end/src/main/uploaded/img/",
         maxFileSize = 1024*1024*50)
-@WebServlet(name = "ContactServlet", value = "/contact")
+@WebServlet(name = "ContactServlet", value = "/contact", loadOnStartup = 0)
 public class ContactServlet extends HttpServlet {
+
+    @Resource(name = "java:comp/env/jdbc/contact")
+    private DataSource dataSource;
+
+    @Override
+    public void init() throws ServletException {
+
+        try {
+            System.out.println(dataSource.getConnection());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("doGet");
